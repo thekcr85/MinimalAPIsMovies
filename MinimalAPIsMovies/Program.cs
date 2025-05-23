74657cb5c3aaa6f29e325b7f9e9b6260440414d5
thekcr85 <thekcr85@gmail.com>
2025-05-23 10:39:57 +0200

    Add project files.

---
+++ MinimalAPIsMovies/Program.cs
@@ -0,0 +1,36 @@
+using MinimalAPIsMovies.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(builder =>
+	{
+		builder.AllowAnyOrigin()
+			.AllowAnyMethod()
+			.AllowAnyHeader();
+	});
+});
+
+var app = builder.Build();
+
+app.UseCors();
+
+app.MapGet("/genres", () =>
+{
+	var genres = new List<Genre> 
+	{
+		new Genre { Id = 1, Name = "Action" },
+		new Genre { Id = 2, Name = "Comedy" },
+		new Genre { Id = 3, Name = "Drama" },
+		new Genre { Id = 4, Name = "Fantasy" },
+		new Genre { Id = 5, Name = "Horror" },
+		new Genre { Id = 6, Name = "Romance" },
+		new Genre { Id = 7, Name = "Sci-Fi" },
+		new Genre { Id = 8, Name = "Thriller" }
+	};
+
+	return Results.Ok(genres);
+});
+
+app.Run();
