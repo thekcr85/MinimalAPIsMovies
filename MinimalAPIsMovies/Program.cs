50a5af1635dd37b00cd48b431050a496f52bd734
thekcr85 <thekcr85@gmail.com>
2025-05-24 16:29:54 +0200

    Add custom extension method MapGenres to endpoints group in Program.cs.

--- MinimalAPIsMovies/Program.cs
+++ MinimalAPIsMovies/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.OutputCaching;
 using Microsoft.EntityFrameworkCore;
 using MinimalAPIsMovies.Data;
+using MinimalAPIsMovies.Endpoints;
 using MinimalAPIsMovies.Models;
 using MinimalAPIsMovies.Repositories;
 
@@ -30,55 +31,6 @@ app.UseCors();
 
 app.UseOutputCache();
 
-var genresEnpoints = app.MapGroup("/genres");
-
-
-
+app.MapGroup("/genres").MapGenres();
 
 app.Run();
-
-
-
-// Endpoints with lambda expressions
-
-//genresEnpoints.MapGet("/", async (IGenreRepository genreRepository) =>
-//{
-//	return await genreRepository.GetAll();
-//}).CacheOutput(c => c.Expire(TimeSpan.FromSeconds(60)).Tag("GetGenres"));
-
-//genresEnpoints.MapGet("/{id}", async (int id, IGenreRepository genreRepository) =>
-//{
-//	var genre = await genreRepository.GetById(id);
-//	return genre is not null ? Results.Ok(genre) : Results.NotFound();
-//});
-
-//genresEnpoints.MapPost("/", async (Genre genre, IGenreRepository genreRepository, IOutputCacheStore outputCacheStore) =>
-//{
-//	var id = await genreRepository.Create(genre);
-//	await outputCacheStore.EvictByTagAsync("GetGenres", default);
-//	return Results.Created($"/genres/{id}", genre);
-//});
-
-//genresEnpoints.MapPut("/{id}", async (int id, Genre genre, IGenreRepository genreRepository, IOutputCacheStore outputCacheStore) =>
-//{
-//	var exists = await genreRepository.Exists(id);
-//	if (!exists)
-//	{
-//		return Results.NotFound();
-//	}
-//	await genreRepository.Update(genre);
-//	await outputCacheStore.EvictByTagAsync("GetGenres", default);
-//	return Results.NoContent();
-//});
-
-//genresEnpoints.MapDelete("/{id}", async (int id, IGenreRepository genreRepository, IOutputCacheStore outputCacheStore) =>
-//{
-//	var exists = await genreRepository.Exists(id);
-//	if (!exists)
-//	{
-//		return Results.NotFound();
-//	}
-//	await genreRepository.Delete(id);
-//	await outputCacheStore.EvictByTagAsync("GetGenres", default);
-//	return Results.NoContent();
-//});
