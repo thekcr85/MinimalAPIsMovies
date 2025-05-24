75780e8604eaf164d2bcdd4c635b4af8ee3f63a0
thekcr85 <thekcr85@gmail.com>
2025-05-24 18:22:29 +0200

    Create IFileStorage interface in Services folder to manage files.

---
+++ MinimalAPIsMovies/Services/IFileStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace MinimalAPIsMovies.Services
+{
+	public interface IFileStorage
+	{
+		Task<string> Store(IFormFile file, string container);
+		Task Delete(string fileName, string container);
+		Task<string> Update(IFormFile file, string fileName, string container);
+	}
+}
