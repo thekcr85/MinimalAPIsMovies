c926b542cbc90dd6e18b9cf648f0d253298e917a
thekcr85 <thekcr85@gmail.com>
2025-05-24 17:08:47 +0200

    Create AutoMapperProfiles class.

---
+++ MinimalAPIsMovies/Utilities/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MinimalAPIsMovies.DTOs;
+using MinimalAPIsMovies.Models;
+
+namespace MinimalAPIsMovies.Utilities
+{
+	public class AutoMapperProfiles : Profile
+	{
+		public AutoMapperProfiles()
+		{
+			CreateMap<Genre, GenreDTO>();
+			CreateMap<CreateGenreDTO, Genre>();
+			CreateMap<Genre, CreateGenreDTO>();
+			CreateMap<GenreDTO, Genre>();
+		}
+	}
+}
