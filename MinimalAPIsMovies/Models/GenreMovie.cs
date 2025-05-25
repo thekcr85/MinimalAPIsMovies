7c1f714c1cfc7e6fafcd9ef6bb30bb861d627f75
thekcr85 <thekcr85@gmail.com>
2025-05-25 19:49:52 +0200

    Create GenreMovie model

---
+++ MinimalAPIsMovies/Models/GenreMovie.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalAPIsMovies.Models
+{
+	public class GenreMovie
+	{
+		public int MovieId { get; set; }
+		public int GenreId { get; set; }
+		public Movie Movie { get; set; } // Navigation property to the Movie entity
+		public Genre Genre { get; set; } // Navigation property to the Genre entity
+	}
+}
