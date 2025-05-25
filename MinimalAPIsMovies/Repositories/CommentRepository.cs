0ac8dd6b7fe2e4feb1085bfd6a2966c062b9384f
thekcr85 <thekcr85@gmail.com>
2025-05-25 17:30:33 +0200

    Extract ICommentRepository interface.

--- MinimalAPIsMovies/Repositories/CommentRepository.cs
+++ MinimalAPIsMovies/Repositories/CommentRepository.cs
@@ -1,6 +1,43 @@
-﻿namespace MinimalAPIsMovies.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIsMovies.Data;
+using MinimalAPIsMovies.DTOs;
+using MinimalAPIsMovies.Models;
+
+namespace MinimalAPIsMovies.Repositories
 {
-	public class CommentRepository
+	public class CommentRepository(ApplicationDbContext context) : ICommentRepository
 	{
+		public async Task<IEnumerable<Comment>> GetAll(int id)
+		{
+			return await context.Comments.Where(c => c.MovieId == id).ToListAsync();
+		}
+
+		public async Task<Comment?> GetById(int id)
+		{
+			return await context.Comments.FirstOrDefaultAsync(c => c.Id == id);
+		}
+
+		public async Task<int> Create(Comment comment)
+		{
+			context.Comments.Add(comment);
+			await context.SaveChangesAsync();
+			return comment.Id;
+		}
+
+		public async Task<bool> Exists(int id)
+		{
+			return await context.Comments.AnyAsync(c => c.Id == id);
+		}
+
+		public async Task Update(Comment comment)
+		{
+			context.Comments.Update(comment);
+			await context.SaveChangesAsync();
+		}
+
+		public async Task Delete(int id)
+		{
+			await context.Comments.Where(c => c.Id == id).ExecuteDeleteAsync();
+		}
 	}
 }
---
+++ MinimalAPIsMovies/Repositories/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using MinimalAPIsMovies.Models;
+
+namespace MinimalAPIsMovies.Repositories
+{
+	public interface ICommentRepository
+	{
+		Task<int> Create(Comment comment);
+		Task Delete(int id);
+		Task<bool> Exists(int id);
+		Task<IEnumerable<Comment>> GetAll(int id);
+		Task<Comment?> GetById(int id);
+		Task Update(Comment comment);
+	}
+}
