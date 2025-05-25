d0dd37509047d4ddbe05066fc8bdc0a80e321824
thekcr85 <thekcr85@gmail.com>
2025-05-25 15:15:08 +0200

    Create MovieRepository class and IMovieRepository interface.

---
+++ MinimalAPIsMovies/Repositories/IMovieRepository.cs
@@ -0,0 +1,14 @@
+﻿using MinimalAPIsMovies.DTOs;
+using MinimalAPIsMovies.Models;
+
+namespace MinimalAPIsMovies.Repositories
+{
+	public interface IMovieRepository
+	{
+		Task<int> Create(Movie movie);
+		Task Delete(int id);
+		Task<IEnumerable<Movie>> GetAll(PaginationDTO paginationDTO);
+		Task<Movie?> GetById(int id);
+		Task Update(Movie movie);
+	}
+}
---
+++ MinimalAPIsMovies/Repositories/MovieRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIsMovies.Data;
+using MinimalAPIsMovies.DTOs;
+using MinimalAPIsMovies.Extensions;
+using MinimalAPIsMovies.Models;
+
+namespace MinimalAPIsMovies.Repositories
+{
+	public class MovieRepository(ApplicationDbContext context, IHttpContextAccessor httpContextAccessor) : IMovieRepository
+	{
+		public async Task<IEnumerable<Movie>> GetAll(PaginationDTO paginationDTO)
+		{
+			var queryable = context.Movies.AsQueryable();
+			await httpContextAccessor.HttpContext!.InsertPaginationParameterInResponseHeader(queryable);
+			return await queryable
+				.OrderBy(m => m.Title)
+				.Paginate(paginationDTO)
+				.ToListAsync();
+		}
+
+		public async Task<Movie?> GetById(int id)
+		{
+			return await context.Movies.FirstOrDefaultAsync(m => m.Id == id);
+		}
+
+		public async Task<int> Create(Movie movie)
+		{
+			context.Movies.Add(movie);
+			await context.SaveChangesAsync();
+			return movie.Id;
+		}
+
+		public async Task Update(Movie movie)
+		{
+			context.Movies.Update(movie);
+			await context.SaveChangesAsync();
+		}
+
+		public async Task Delete(int id)
+		{
+			await context.Movies.Where(m => m.Id == id).ExecuteDeleteAsync();
+		}
+	}
+}
