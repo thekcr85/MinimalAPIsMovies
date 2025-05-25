1d4ed31784d49c080d533f300d0443a54b7baa13
thekcr85 <thekcr85@gmail.com>
2025-05-25 19:45:09 +0200

    Update MovieDTO

--- MinimalAPIsMovies/DTOs/MovieDTO.cs
+++ MinimalAPIsMovies/DTOs/MovieDTO.cs
@@ -7,5 +7,6 @@
 		public bool InTheaters { get; set; }
 		public DateTime ReleaseDate { get; set; }
 		public string? Poster { get; set; } // Nullable to allow for movies without posters
+		public IEnumerable<CommentDTO> Comments { get; set; } = new List<CommentDTO>(); // Initialize to an empty list to avoid null reference issues
 	}
 }
