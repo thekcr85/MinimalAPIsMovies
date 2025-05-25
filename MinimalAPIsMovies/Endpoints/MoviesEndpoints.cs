9e5620aa0b2d7fe5fa15def05edf7f04547d14da
thekcr85 <thekcr85@gmail.com>
2025-05-25 15:24:48 +0200

    Add CreateMovieDTO and MovieDTO classes. Create AutoMapper profiles for Movie entity.

---
+++ MinimalAPIsMovies/Endpoints/MoviesEndpoints.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using MinimalAPIsMovies.DTOs;
+using MinimalAPIsMovies.Repositories;
+using System.Runtime.CompilerServices;
+
+namespace MinimalAPIsMovies.Endpoints
+{
+	public static class MoviesEndpoints
+	{
+		public static RouteGroupBuilder MapMovies(this RouteGroupBuilder group)
+		{
+			return group;
+		}
+
+		static async Task<Ok<IEnumerable<MovieDTO>> GetMovies(IMovieRepository movieRepository, IMapper mapper, PaginationDTO paginationDTO)>
+	}
+}
--- MinimalAPIsMovies/Utilities/AutoMapperProfiles.cs
+++ MinimalAPIsMovies/Utilities/AutoMapperProfiles.cs
@@ -15,5 +15,10 @@ public class AutoMapperProfiles : Profile
 		CreateMap<Actor, CreateActorDTO>().ReverseMap()
 			.ForMember(p => p.Picture, options => options.Ignore()); // Ignore the Picture property when mapping
 																	 // from CreateActorDTO to Actor, as it will be handled separately
+
+		CreateMap<Movie, MovieDTO>().ReverseMap();
+		CreateMap<Movie, CreateMovieDTO>().ReverseMap()
+			.ForMember(p => p.Poster, options => options.Ignore()); // Ignore the Poster property when mapping
+																	// from CreateMovieDTO to Movie, as it will be handled separately
 	}
 }
