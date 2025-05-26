9060b324d720351cda930ff8843cefd5d2b918e3
thekcr85 <thekcr85@gmail.com>
2025-05-26 14:35:58 +0200

    Update CreateGenreDTOValidator

--- MinimalAPIsMovies/Validations/CreateGenreDTOValidator.cs
+++ MinimalAPIsMovies/Validations/CreateGenreDTOValidator.cs
@@ -8,7 +8,15 @@ namespace MinimalAPIsMovies.Validations
 		public CreateGenreDTOValidator()
 		{
 			RuleFor(x => x.Name)
-				.NotEmpty();
+				.NotEmpty().WithMessage("The field {PropertyName} is required")
+				.MaximumLength(50).WithMessage("The field {PropertyName} must not exceed {MaxLength} characters")
+				.Must(FirstLetterIsUpperCase).WithMessage("The field {PropertyName} should start with uppercase");
 		}
+
+		private static bool FirstLetterIsUpperCase(string value)
+		{
+			return string.IsNullOrEmpty(value) || char.IsUpper(value[0]);
+		}
+
 	}
 }
