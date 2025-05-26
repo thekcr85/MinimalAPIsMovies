aefac117733309fdb87bdcb6a0f3ba557c2a16d0
thekcr85 <thekcr85@gmail.com>
2025-05-26 11:36:26 +0200

    Add Assign method signature

--- MinimalAPIsMovies/Repositories/IMovieRepository.cs
+++ MinimalAPIsMovies/Repositories/IMovieRepository.cs
@@ -6,6 +6,7 @@ namespace MinimalAPIsMovies.Repositories
 	public interface IMovieRepository
 	{
 		Task Assign(int id, List<int> genresIds);
+		Task Assign(int id, List<ActorMovie> actors);
 		Task<int> Create(Movie movie);
 		Task Delete(int id);
 		Task<bool> Exists(int id);
