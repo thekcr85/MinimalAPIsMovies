36094506bdd5d3c77726ab50b108e1dc3ae61818
thekcr85 <thekcr85@gmail.com>
2025-05-26 11:19:30 +0200

    Create ActorMovie model

---
+++ MinimalAPIsMovies/Models/ActorMovie.cs
@@ -0,0 +1,12 @@
+﻿namespace MinimalAPIsMovies.Models
+{
+	public class ActorMovie
+	{
+		public int ActorId { get; set; } // Foreign key to Actor
+		public int MovieId { get; set; } // Foreign key to Movie
+		public Actor Actor { get; set; } = null!; // Navigation property to Actor
+		public Movie Movie { get; set; } = null!; // Navigation property to Movie
+		public int Order { get; set; } 
+		public string Character { get; set; } = null!; 
+	}
+}
