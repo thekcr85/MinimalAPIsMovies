f54739908ee11e6d188090c93754eb854180a560
thekcr85 <thekcr85@gmail.com>
2025-05-26 11:23:36 +0200

    Update Movie model

--- MinimalAPIsMovies/Models/Movie.cs
+++ MinimalAPIsMovies/Models/Movie.cs
@@ -7,7 +7,8 @@
 		public bool InTheaters { get; set; }
 		public DateTime ReleaseDate { get; set; }
 		public string? Poster { get; set; }
-		public ICollection<Comment> Comments { get; set; } = new List<Comment>(); // Navigation property
-		public ICollection<GenreMovie> GenresMovies { get; set; } = new List<GenreMovie>(); // Navigation property for the many-to-many relationship
+		public ICollection<Comment> Comments { get; set; } = new List<Comment>(); // Navigation property for comments related to the movie
+		public ICollection<GenreMovie> GenresMovies { get; set; } = new List<GenreMovie>(); // Navigation property for the many-to-many relationship with Genre
+		public ICollection<ActorMovie> ActorsMovies { get; set; } = new List<ActorMovie>(); // Navigation property for the many-to-many relationship with Actor
 	}
 }
