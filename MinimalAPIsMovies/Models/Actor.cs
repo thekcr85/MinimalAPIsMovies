43d4fbde1355c31e231e2f815cb364e4b6b9e432
thekcr85 <thekcr85@gmail.com>
2025-05-26 11:22:22 +0200

    Update Actor model

--- MinimalAPIsMovies/Models/Actor.cs
+++ MinimalAPIsMovies/Models/Actor.cs
@@ -6,5 +6,6 @@
 		public string Name { get; set; } = null!;
 		public DateTime DateOfBirth { get; set; }
 		public string? Picture { get; set; }
+		public ICollection<ActorMovie> ActorsMovies { get; set; } = new List<ActorMovie>(); // Navigation property to ActorMovie
 	}
 }
