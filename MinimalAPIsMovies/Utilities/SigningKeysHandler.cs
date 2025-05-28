54e995385841710f82f7a71913f016a2c80c458f
thekcr85 <thekcr85@gmail.com>
2025-05-28 09:14:07 +0200

    Add SigningKeysHandler

---
+++ MinimalAPIsMovies/Utilities/SigningKeysHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalAPIsMovies.Utilities
+{
+	public static class SigningKeysHandler
+	{
+		public const string DefaultIssuer = "my-app";
+		private const string SigningKeysConfigSection = "Authentication:Schemes:Bearer:SigningKeys";
+		private const string ConfigKeyIssuer = "Issuer";
+		private const string ConfigKeyValue = "Value";
+	}
+}
